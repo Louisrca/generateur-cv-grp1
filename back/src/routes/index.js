55a3e94e6463fced7c49742631ce3fb633820f85
NicolasBELLANGER <nicobell93220@gmail.com>
2024-11-24 13:56:19 +0100

    Ajustement Routes

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
-const bookRoutes = require('./curriculum');
+const curriculumRoutes = require('./curriculum');
 const authRoutes = require('./auth');
 const userRoutes = require('./users');
 const uploadRoutes = require('./uploadImage');
 
 app.use('/users', userRoutes);
-app.use('/curriculum', bookRoutes);
+app.use('/curriculum', curriculumRoutes);
 app.use('/auth', authRoutes);
 app.use('/upload', uploadRoutes);
 
