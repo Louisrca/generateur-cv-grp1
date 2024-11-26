8edaf642fa02194658c57ca41283a1706ab07e52
NicolasBELLANGER <nicobell93220@gmail.com>
2024-11-26 16:00:50 +0100

    Delete HomePage

--- web/src/pages/Home/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-import { NavBar } from "../../composant/NavBar/NavBar";
-import styles from "./Home.module.css";
-export default function HomePage() {
-  return (
-    <div className={styles.homeContainer}>
-      <NavBar />
-    </div>
-  );
-}
