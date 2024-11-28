71108ac578d3da5842082ea9086374566a18d73c
Louis <roccalouis26@gmail.com>
2024-11-28 22:11:51 +0100

    style

--- web/src/pages/UserCurriculum/UserCurriculum.jsx
+++ web/src/pages/UserCurriculum/UserCurriculum.jsx
@@ -1,19 +1,24 @@
 import NavBar from "../../composant/NavBar/NavBar";
 import { ShowUserCurriculum } from "../../composant/ShowUserCurriculum/ShowUserCurriculum";
+import { Button } from "react-bootstrap";
 export default function UserCurriculum() {
   return (
     <div>
       <NavBar />
-      <h1
-        style={{
-          fontWeight: "bold",
-          marginBottom: "4rem",
-          marginLeft: "4rem",
-          marginTop: "2rem",
-        }}
-      >
-        Mes Curriculum
-      </h1>
+      <div>
+        <h1
+          style={{
+            fontWeight: "bold",
+            marginBottom: "4rem",
+            marginLeft: "4rem",
+            marginTop: "2rem",
+          }}
+        >
+          Mes Curriculum
+        </h1>
+        <Button href="/createcv">Créer un CV</Button>
+      </div>
+
       <ShowUserCurriculum />
     </div>
   );
