bb7a8ab63b82098fb2fb3f51dfa35d03b58f2ca1
Louis <roccalouis26@gmail.com>
2024-11-29 14:34:17 +0100

    add export pdf

--- web/src/composant/CurriculumView/CurriculumView.jsx
+++ web/src/composant/CurriculumView/CurriculumView.jsx
@@ -2,28 +2,42 @@ import styles from "./CurriculumView.module.css";
 import { v4 as uuidv4 } from "uuid";
 import { Button } from "react-bootstrap";
 import { useDeleteCurriculum } from "../../api/curriculum/curriculum";
+import { useRef, createRef } from "react";
 
 import PropTypes from "prop-types";
 import { Edit, Delete } from "@mui/icons-material";
+import html2pdf from "html2pdf.js";
 
 export const CurriculumView = ({ userCurriculum }) => {
   const { mutate: deleteCurriculum } = useDeleteCurriculum();
-
+  const cvRefs = useRef(userCurriculum.map(() => createRef()));
   const handleDelete = (cvId) => {
     if (window.confirm("Veux-tu vraiment supprimer le CV ?")) {
       deleteCurriculum(cvId);
     }
   };
 
+  const handleExportPDF = (index) => {
+    const element = cvRefs.current[index]; // Référence à la div contenant le CV
+
+    const options = {
+      filename: `cv_${uuidv4()}.pdf`, // Nom du fichier PDF exporté
+      image: { type: "jpeg", quality: 1 },
+      html2canvas: { scale: 2 }, // Pour une meilleure qualité d'image
+      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+    };
+
+    html2pdf().from(element.current).set(options).save(); // Convertir la div en PDF
+  };
   const curriculumData = Array.isArray(userCurriculum)
     ? userCurriculum
     : [userCurriculum];
 
   return (
     <>
-      {curriculumData.map((curriculum) => (
-        <div key={curriculum._id} className={styles.mainContainer}>
-          <div className={styles.buttonAction}>
+      {curriculumData.map((curriculum, index) => (
+        <div key={curriculum._id}>
+          <div className={styles.mainContainer}>
             <Button href={"edit-cv/" + curriculum?._id}>
               {" "}
               <Edit />
@@ -34,103 +48,143 @@ export const CurriculumView = ({ userCurriculum }) => {
             >
               <Delete />
             </Button>
+            <Button variant="secondary" onClick={() => handleExportPDF(index)}>
+              Exporter en PDF
+            </Button>
           </div>
 
-          <div key={uuidv4()} className={styles.container}>
-            <div className={styles.sidebar}>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  marginBottom: "1.5rem",
-                  marginTop: "1.5rem",
-                }}
-              >
-                {curriculum?.imageUrl !== "/default-image.jpg" ? (
-                  <img
-                    style={{
-                      width: "6rem",
-                      height: "6rem",
-                      borderRadius: "50%",
-                    }}
-                    src={curriculum?.imageUrl}
-                    alt="profile"
-                  />
-                ) : (
-                  <img
-                    style={{
-                      width: "6rem",
-                      height: "6rem",
-                      borderRadius: "50%",
-                    }}
-                    src="./image.png"
-                    alt="profile"
-                  />
-                )}
+          <div ref={cvRefs.current[index]} key={curriculum._id}>
+            <div key={uuidv4()} className={styles.container}>
+              <div className={styles.sidebar}>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginBottom: "1.5rem",
+                    marginTop: "1.5rem",
+                  }}
+                >
+                  {curriculum?.imageUrl !== "/default-image.jpg" ? (
+                    <img
+                      style={{
+                        width: "6rem",
+                        height: "6rem",
+                        borderRadius: "50%",
+                      }}
+                      src={curriculum?.imageUrl}
+                      alt="profile"
+                    />
+                  ) : (
+                    <img
+                      style={{
+                        width: "6rem",
+                        height: "6rem",
+                        borderRadius: "50%",
+                      }}
+                      src="./image.png"
+                      alt="profile"
+                    />
+                  )}
+                </div>
+
+                <h2>
+                  {curriculum?.name} {curriculum?.lastname}
+                </h2>
+
+                <p>Email: {curriculum?.email}</p>
+                <p>Téléphone: {curriculum?.phone}</p>
+
+                <h2>Compétences Techniques</h2>
+                <ul>
+                  {curriculum?.technicalSkills?.map((technicalSkills) => (
+                    <>
+                      <span key={uuidv4()} style={{ fontWeight: "bold" }}>
+                        {technicalSkills?.category}
+                      </span>
+                      {technicalSkills?.skills?.map((skill) => (
+                        <li key={skill.name}>
+                          {skill?.name} ({skill?.level})
+                        </li>
+                      ))}
+                    </>
+                  ))}
+                </ul>
+
+                <h2>Soft Skills</h2>
+                <ul>
+                  {curriculum?.skills?.map((skills) => (
+                    <>
+                      <span
+                        key={uuidv4()}
+                        style={{ display: "flex", flexDirection: "column" }}
+                      >
+                        {skills}
+                      </span>
+                    </>
+                  ))}
+                </ul>
+
+                <h2>{"Centres d'intérêt"}</h2>
+                <ul>
+                  {curriculum?.areaOfInterests?.map((areaOfInterests) => (
+                    <>
+                      <span
+                        key={uuidv4()}
+                        style={{ display: "flex", flexDirection: "column" }}
+                      >
+                        {areaOfInterests}
+                      </span>
+                    </>
+                  ))}
+                </ul>
               </div>
 
-              <h2>
-                {curriculum?.name} {curriculum?.lastname}
-              </h2>
+              <div className={styles.main}>
+                <h1>{curriculum?.jobTitle}</h1>
+                <p>{curriculum?.description}</p>
 
-              <p>Email: {curriculum?.email}</p>
-              <p>Téléphone: {curriculum?.phone}</p>
+                <h2 className={styles.highlight}>Expérience Professionnelle</h2>
+                <div className={styles.section}>
+                  <div className="experience-item">
+                    {curriculum?.experiences?.map((experience) => {
+                      const startDate = new Date(experience.startYear);
+                      const endDate = new Date(experience.endYear);
+                      const formattedStartDate = startDate.toLocaleDateString(
+                        "fr-FR",
+                        {
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric",
+                        }
+                      );
 
-              <h2>Compétences Techniques</h2>
-              <ul>
-                {curriculum?.technicalSkills?.map((technicalSkills) => (
-                  <>
-                    <span key={uuidv4()} style={{ fontWeight: "bold" }}>
-                      {technicalSkills?.category}
-                    </span>
-                    {technicalSkills?.skills?.map((skill) => (
-                      <li key={skill.name}>
-                        {skill?.name} ({skill?.level})
-                      </li>
-                    ))}
-                  </>
-                ))}
-              </ul>
+                      const formattedEndDate = endDate.toLocaleDateString(
+                        "fr-FR",
+                        {
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric",
+                        }
+                      );
+                      return (
+                        <div key={uuidv4()}>
+                          <h3>{experience?.title}</h3>
+                          <span>{experience?.company}</span>
+                          <span>
+                            {formattedStartDate} - {formattedEndDate}
+                          </span>
+                          <p>{experience?.description}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
 
-              <h2>Soft Skills</h2>
-              <ul>
-                {curriculum?.skills?.map((skills) => (
-                  <>
-                    <span
-                      key={uuidv4()}
-                      style={{ display: "flex", flexDirection: "column" }}
-                    >
-                      {skills}
-                    </span>
-                  </>
-                ))}
-              </ul>
-
-              <h2>{"Centres d'intérêt"}</h2>
-              <ul>
-                {curriculum?.areaOfInterests?.map((areaOfInterests) => (
-                  <>
-                    <span
-                      key={uuidv4()}
-                      style={{ display: "flex", flexDirection: "column" }}
-                    >
-                      {areaOfInterests}
-                    </span>
-                  </>
-                ))}
-              </ul>
-            </div>
-
-            <div className={styles.main}>
-              <h1>{curriculum?.jobTitle}</h1>
-              <p>{curriculum?.description}</p>
-
-              <h2 className={styles.highlight}>Expérience Professionnelle</h2>
-              <div className={styles.section}>
-                <div className="experience-item">
-                  {curriculum?.experiences?.map((experience) => {
-                    const startDate = new Date(experience.startYear);
-                    const endDate = new Date(experience.endYear);
+                <h2 className={styles.highlight}>Formation</h2>
+                <div className={styles.section}>
+                  {curriculum?.educations?.map((education) => {
+                    const startDate = new Date(education.startYear);
+                    const endDate = new Date(education.endYear);
                     const formattedStartDate = startDate.toLocaleDateString(
                       "fr-FR",
                       {
@@ -150,48 +204,16 @@ export const CurriculumView = ({ userCurriculum }) => {
                     );
                     return (
                       <div key={uuidv4()}>
-                        <h3>{experience?.title}</h3>
-                        <span>{experience?.company}</span>
+                        <h3>{education?.degree}</h3>
+                        <span>{education?.school}</span>
                         <span>
                           {formattedStartDate} - {formattedEndDate}
                         </span>
-                        <p>{experience?.description}</p>
+                        <p>{education?.fieldOfStudy}</p>
                       </div>
                     );
                   })}
                 </div>
-              </div>
-
-              <h2 className={styles.highlight}>Formation</h2>
-              <div className={styles.section}>
-                {curriculum?.educations?.map((education) => {
-                  const startDate = new Date(education.startYear);
-                  const endDate = new Date(education.endYear);
-                  const formattedStartDate = startDate.toLocaleDateString(
-                    "fr-FR",
-                    {
-                      year: "numeric",
-                      month: "long",
-                      day: "numeric",
-                    }
-                  );
-
-                  const formattedEndDate = endDate.toLocaleDateString("fr-FR", {
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                  });
-                  return (
-                    <div key={uuidv4()}>
-                      <h3>{education?.degree}</h3>
-                      <span>{education?.school}</span>
-                      <span>
-                        {formattedStartDate} - {formattedEndDate}
-                      </span>
-                      <p>{education?.fieldOfStudy}</p>
-                    </div>
-                  );
-                })}
               </div>
             </div>
           </div>
