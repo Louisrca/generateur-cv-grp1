4732b6c0f796f5c36e0365dc441c5ef1cbf1e7ac
Louis <roccalouis26@gmail.com>
2024-11-29 01:43:58 +0100

    style update

--- web/src/composant/AddRecommendation/AddRecommendation.jsx
+++ web/src/composant/AddRecommendation/AddRecommendation.jsx
@@ -1,6 +1,8 @@
 import { useCreateRecommendation } from "../../api/recommendation/recommendation";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
+import { Button } from "react-bootstrap";
+import styles from "./AddRecommendation.module.css";
 
 export default function AddRecommendation() {
   const { cvId } = useParams();
@@ -18,13 +20,16 @@ export default function AddRecommendation() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="message"
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button type="submit">publier </button>
+      <form onSubmit={handleSubmit} className={styles.recommendationForm}>
+        <div className={styles.textareaContainer}>
+          <textarea
+            className={styles.customTextarea}
+            placeholder="Ajoute un commentaire..."
+            name="message"
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+        </div>
+        <Button type="submit">publier </Button>
       </form>
     </div>
   );
--- web/src/pages/Curriculum/Curriculum.jsx
+++ web/src/pages/Curriculum/Curriculum.jsx
@@ -4,14 +4,26 @@ import NavBar from "../../composant/NavBar/NavBar";
 import { ShowRecommendation } from "../../composant/ShowRecommendation/ShowRecommendation";
 import { CurriculumPreview } from "../../composant/CurriculumPreview/CurriculumPreview";
 import { useGetCurriculumById } from "../../api/curriculum/curriculum";
+import { CircularProgress } from "@mui/material";
 import styles from "./Curriculum.module.css";
 
 export default function Curriculum() {
   const param = useParams();
   const cvId = param.cvId;
   console.log("🚀 ~ Curriculum ~ cvId:", cvId);
-  const { data: curriculumDetails } = useGetCurriculumById(cvId);
-  console.log("🚀 ~ Curriculum ~ curriculumDetails:", curriculumDetails);
+  const {
+    data: curriculumDetails,
+    isLoading,
+    isError,
+  } = useGetCurriculumById(cvId);
+
+  if (isLoading) {
+    return <CircularProgress />;
+  }
+
+  if (isError) {
+    return <div>Erreur lors du chargement du CV</div>;
+  }
 
   return (
     <div>
